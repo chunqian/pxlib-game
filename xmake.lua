5d8abcf33a623cc1f7e3d4b4ef4e7a625aaddafd
chunqian.shen <chunqian.shen@qq.com>
2021-01-28 15:31:06 +0800

    Update.

--- xmake.lua
+++ xmake.lua
@@ -3,8 +3,8 @@ add_rules("mode.release", "mode.debug")
 pxlibdir = "$(projectdir)/pxlib"
 
 includes("project/windows/pxlib.lua")
-includes("project/windows/demo.lua")
 includes("project/windows/client_startup.lua")
-includes("project/windows/client_game.lua")
-includes("project/windows/server_startup.lua")
-includes("project/windows/server_game.lua")
+-- includes("project/windows/client_game.lua")
+-- includes("project/windows/server_startup.lua")
+-- includes("project/windows/server_game.lua")
+-- includes("project/windows/demo.lua")
